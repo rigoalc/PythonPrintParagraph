0751cfeb3a3d5e480c833b32cd2bf146f674a215
Rigo Alcover <rodigomartinalcover@gmail.com>
2021-09-09 22:11:35 -0400

    Create Print Paragraph.py

---
+++ Print Paragraph.py
@@ -0,0 +1,24 @@
+# Rodrigo Alcover
+# 08/09/2021
+# CIS-216-12292 
+# Python Programming
+
+
+print('''
+Hello world, my name is Rodrigo Alcover 40 years old, and born in Buenos Aires, Argentina.
+
+I'm a KVCC student for the last three years, seeking a software development degree. 
+
+I have taken Programming logic, Java, and SQL classes. These little experiences make me feel or tend to front-end development, 
+so after a meeting with a counselor, I have decided to study Python as a programming Language requirement for my major.
+
+I would like to further my understandings of the concepts that I have learned during these past 2 years and use it to start my career.
+''')
+
+# The development time was about 3 hours total because I needed to install homebrew and python extensions for the first time.
+
+# This program prompt my introduction and a brief idea of why im taking Python class.
+
+
+
+
